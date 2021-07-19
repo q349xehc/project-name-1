2ae1b0b6c2689013d738e94086800795d4554edf
pavel.gnedov <pavel.gnedov@eltex.loc>
2021-07-19 19:38:57 +0700

    Вернул AppService.getWorkTime как было

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,21 +12,19 @@ export class AppService {
   }
   async getWorkTime(useridParam: number, dateParam: string /* string в формате 'YYYY-MM-DD' */): Promise<number> {
     var z=0;
-    axios.get(`http://admin:thuvj1sh@dev-redmine.gnedov.info:8380/time_entries.json?user_id=${useridParam}&spent_on=${dateParam}`).then(function (response) {
-    
-      var timeEntries = response.data.time_entries;
-      for (var i = 1; i < timeEntries.length; i++) {
-        var timeItem = timeEntries[i];
-        var user = timeItem.user;
-        var userid = user.id;
-        var hours = timeItem.hours;
-        var Date = timeItem.spent_on;
-        console.log(i, ':', 'userid =', userid, '; Hours =', hours, '; Date =', Date);
-        z=z+timeItem.hours;
-        console.log(z);
-      }
-    })
-
+    // poshli v main
+    const response = await axios.get(`http://admin:thuvj1sh@dev-redmine.gnedov.info:8380/time_entries.json?user_id=${useridParam}&spent_on=${dateParam}`)
+    const timeEntries = response.data.time_entries;
+    for (var i = 1; i < timeEntries.length; i++) {
+      var timeItem = timeEntries[i];
+      var user = timeItem.user;
+      var userid = user.id;
+      var hours = timeItem.hours;
+      var Date = timeItem.spent_on;
+      console.log(i, ':', 'userid =', userid, '; Hours =', hours, '; Date =', Date);
+      z=z+timeItem.hours;
+      console.log(z);
+    }
     return z;
   }
 }
