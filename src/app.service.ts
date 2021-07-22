07aca0cfb1f2e4dff8404369c1a50746c4893392
pavel.gnedov <pavel.gnedov@eltex.loc>
2021-07-22 18:32:43 +0700

    Улушение кода

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,29 +5,33 @@ import { config } from './config-reader';
 
 @Injectable()
 export class AppService {
+  
   getHello(): string {
     return 'Паш, мы почти смогли, или не очень';
   }
-  async getWorkTime(useridParam: number, dateParam: string /* string в формате 'YYYY-MM-DD' */): Promise<ResponseType> {
-    var z=0;
-    // TODO: заменить часть url-а на получаемую из конфига в вызове функции axios.get -
-    // перед этим если нужен какой-то ещё вспомогательный код, то тоже можно писать и себя не
-    // ограничивать в строчках кода
-    // смотреть в каменты в config-reader.ts
+  
+  /**
+   * Получение рабочего времени из redmine
+   * @param useridParam 
+   * @param dateParam 
+   * @returns 
+   */
+  async getWorkTime(useridParam: number, dateParam: string): Promise<ResponseType> {
+    let sumTime = 0;
     const response = await axios.get(config.redmineUrl + `/time_entries.json?user_id=${useridParam}&spent_on=${dateParam}`);
     const timeEntries = response.data.time_entries;
     for (var i = 1; i < timeEntries.length; i++) {
       var timeItem = timeEntries[i];
       var user = timeItem.user;
-      z=z+timeItem.hours;
+      sumTime = sumTime + timeItem.hours;
     }
-    var daywork: ResponseType = {
+    const result: ResponseType = {
       Redmineusername: user.name,
       userId: useridParam,
       date: dateParam,
-      workTime: z
+      workTime: sumTime
     }
-    return daywork;
+    return result;
   }
 }
 
--- src/config-reader.ts
+++ src/config-reader.ts
@@ -3,24 +3,10 @@ import { readFileSync } from "fs";
 class ConfigReader {
 	redmineUrl: string;
 	constructor() {
-		var Configietoslojno;
         const rawConfig: string = readFileSync('./config.json', {encoding: 'utf8'});
-        Configietoslojno = JSON.parse(rawConfig);
-		this.redmineUrl = Configietoslojno.redmineUrl;
-		
-		
-
-		// TODO: Надо rawConfig распарсить (преобразовать) в объект
-		// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON#methods - краткая инфа
-		// https://learn.javascript.ru/json - всё очень подробно
-		// 
-		// т.е надо:ё
-		// ... // что-то тут ещё можно сделать, а потом:
-		// this.redmineUrl = ...
+        const parsedConfig = JSON.parse(rawConfig);
+		this.redmineUrl = parsedConfig.redmineUrl;
 	}
 }
 
-export const config = new ConfigReader(); // config можно загружать в другие части сервиса с помощью:
-										  // import { config } from './config-reader'
-										  // потом из config можно использовать redmineUrl с помощью:
-										  // config.redmineUrl
+export const config = new ConfigReader();
